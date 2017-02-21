f5d486582c98223facbe3e4cf7ea5b80d3fed334
Brett O'Daniell <brettlod@yahoo.com>
2017-02-20 20:14:41 -0600

    updated username_sort for Blogz and updated blog posts

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ class BlogHandler(webapp2.RequestHandler):
         """
 
         # TODO - filter the query so that only posts by the given user
-        return None
+        query = Post.all().filter("author", user).order('-created')
+        return query.fetch(limit=limit, offset=offset)
+#        return None
 
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
@@ -260,10 +262,10 @@ class LoginHandler(BlogHandler):
 
     # TODO - The login code here is mostly set up for you, but there isn't a template to log in
 
-    def render_login_form(self, error=""):
+    def render_login_form(self, username = "", error="", errors=""):
         """ Render the login form with or without an error, based on parameters """
         t = jinja_env.get_template("login.html")
-        response = t.render(error=error)
+        response = t.render(error=error, errors=errors)
         self.response.out.write(response)
 
     def get(self):
